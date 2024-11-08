a0451202944eed3c2fba83a42350f94ef0621768
giovalim <168843878+giovalim@users.noreply.github.com>
2024-11-08 14:30:08 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'História/Exploração Espacial.',
+    'Em que ano o homem pisou na Lua pela primeira vez?',
+    '1969.'
+)
+
+criaCartao(
+    'Direito/História',
+    'O que é a Constituição de 1988 no Brasil?',
+    'A Constituição de 1988 é a atual constituição do Brasil, que estabeleceu os direitos e deveres dos cidadãos, além de organizar o funcionamento dos poderes do Estado.'
+)
+
+criaCartao(
+    'Física',
+    'O que é a teoria da relatividade de Albert Einstein?',
+    'A teoria da relatividade propõe que as leis da física são as mesmas para todos os observadores, independentemente de sua velocidade ou direção, e descreve como a gravidade pode curvar o espaço-tempo.'
+)
+
+criaCartao(
+    'História',
+    'Quem foi o líder político e militar responsável pela independência da Argentina?',
+    ' José de San Martín.'
+)
